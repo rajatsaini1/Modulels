48935448d49245822653f7a8b4df7d2503631906
Rajat Saini <51861284+rajatsaini1@users.noreply.github.com>
2022-01-27 15:58:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,21 @@
 terraform {
   cloud {
     organization = "rajats"
-
+  
     workspaces {
       name = "Modules"
     }
+    
   }
-}
-
-required_providers {
+  required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "3.73.0"
     }
   }
+}
+
+
 
 provider "aws" {
   region = "ap-south-1"
