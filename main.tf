e16a8805355c868a783440c05c9fce3c9306fa7c
Rajat Saini <51861284+rajatsaini1@users.noreply.github.com>
2022-01-31 16:40:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,14 +15,15 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
-
+/*
 module "ec2module" {
  source = ".//Modules/EC2" 
   instance_type = var.instance_type
 }
-/*
+*/
 module "mys3" {
   source = ".//Modules/s3" 
-
+  tags = {
+    Name = var.name
+  }
 } 
-*/
