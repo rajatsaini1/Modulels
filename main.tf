7fa5a71d5b4e5234cc3f1f51865f6203106c42d1
Rajat Saini <51861284+rajatsaini1@users.noreply.github.com>
2022-02-09 15:54:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,25 +9,25 @@ terraform {
   }
 
   cloud {
-+   organization = "rajats"
+  organization = "rajats"
 
     workspaces {
       name = "gh-actions-demo"
     }
   }
 }
-/*
+
 module "ec2module" {
  source = ".//Modules/EC2" 
   instance_type = var.instance_type
 }
-*/
 
 
+/*
 module "mys3" {
     bucket     = var.bucket
     source     = ".//Modules/s3" 
     name       = var.name
     acl        = var.acl
     
-} 
+} */
