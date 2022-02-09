567339cdd2260ba88ac680eaad4a5bf745b688fe
Rajat Saini <51861284+rajatsaini1@users.noreply.github.com>
2022-02-09 16:51:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,15 +10,12 @@ terraform {
 
 module "ec2module" {
  source = ".//Modules/EC2" 
-  instance_type = var.instance_type
 }
 
 
 /*
 module "mys3" {
-    bucket     = var.bucket
-    source     = ".//Modules/s3" 
-    name       = var.name
-    acl        = var.acl
+   
+    source     = ".//Modules/s3"
     
 } */
