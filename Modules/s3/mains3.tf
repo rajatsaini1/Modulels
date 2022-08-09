411aed2e72a18a84517f25facb14e8c5080370c3
“velotioaastha” <aastha@knotch.com>
2022-08-09 22:53:46 +0530

    formatted

--- Modules/s3/mains3.tf
+++ Modules/s3/mains3.tf
@@ -1,12 +1,12 @@
-resource "aws_s3_bucket" "prod_website" {  
-    acl    = var.acl   
-    bucket = var.bucket
-    website {    
-    index_document = "index.html"    
-    error_document = "error.html"   
+resource "aws_s3_bucket" "prod_website" {
+  acl    = var.acl
+  bucket = var.bucket
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
   }
-    tags = {
-    Name        = var.name
+  tags = {
+    Name = var.name
   }
 }
 
@@ -14,16 +14,14 @@ resource "aws_s3_bucket_object" "index" {
 
   bucket = aws_s3_bucket.prod_website.id
 
-  key    = "index.html"
-  source = ".//Modules/s3/index.html"
+  key          = "index.html"
+  source       = ".//Modules/s3/index.html"
   content_type = "text/html"
 
-} 
+}
 
-
-
-resource "aws_s3_bucket_policy" "prod_website" {  
-  bucket = aws_s3_bucket.prod_website.id   
+resource "aws_s3_bucket_policy" "prod_website" {
+  bucket = aws_s3_bucket.prod_website.id
   policy = <<POLICY
 {
     "Version": "2012-10-17",
